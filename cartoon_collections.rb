d2be377326ab83b62d7f49be6a0cf973c499dd4f
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-09-29 20:49:17 -0700

    Completed Assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+# require 'pry'
+
+def roll_call_dwarves dwarves
+  dwarves.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet words
+  words.map{ |word| word.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls list
+  found = list.select { |call| call.length > 4 }
+  found.length > 0 ? true : false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese food_items
   cheese_types = ["cheddar", "gouda", "camembert"]
+  found_food = food_items.select { |item| cheese_types.include?(item) }
+  found_food.pop
 end
